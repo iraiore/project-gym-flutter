fb7185b78004184c0e68c0e8f9f0023f02f0c485
Iraiore <yagoiraiore@gmail.com>
2023-11-12 23:24:14 -0400

    foi feita a conexao com o firebase. Noi foi feito o merge para main

--- flutter_gymapp/lib/componentes/decoracao_campo_autenticacao.dart
+++ flutter_gymapp/lib/componentes/decoracao_campo_autenticacao.dart
@@ -9,22 +9,22 @@ InputDecoration getAuthenticationInputDecoration(String Label) {
     contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
     border: OutlineInputBorder(
       borderRadius: BorderRadius.circular(20),
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20),
-        borderSide: BorderSide(color: Colors.black, width: 2),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20),
-        borderSide: const BorderSide(color: MinhasCores.laranja1, width: 4),
-      ),
-      errorBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20),
-        borderSide: const BorderSide(color: Colors.red, width: 2),
-      ),
-      focusedErrorBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20),
-        borderSide: const BorderSide(color: Colors.red, width: 4),   
-      ),   
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: BorderSide(color: Colors.black, width: 2),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: MinhasCores.laranja1, width: 4),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: Colors.red, width: 2),
+    ),
+    focusedErrorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: Colors.red, width: 4),
+    ),
   );
 }
